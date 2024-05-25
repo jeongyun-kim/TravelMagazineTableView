a9fa0c38c317e4cf419ac242ecd23dc3f357a2cf
jeongyun-kim <kkanyo04@naver.com>
2024-05-26 01:37:35 +0900

    셀 선택 시 해당 셀의 링크를 불러와 사파리로 이동하기 구현 / DateFormatter를 이용해 날짜 변환 후 셀에 표현

--- TableView_Travel/MagazineCell.swift
+++ TableView_Travel/MagazineCell.swift
@@ -16,22 +16,34 @@ class MagazineCell: UITableViewCell {
     
     func configure(title: String, subtitle: String, date: String, imageUrl: String) {
         magazineTitleLabel.text = title
-        magazineTitleLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        magazineTitleLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
         magazineTitleLabel.numberOfLines = 2
         
         magazineSubTitleLabel.text = subtitle
-        magazineSubTitleLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        magazineSubTitleLabel.font = UIFont.systemFont(ofSize: 15, weight: .bold)
         magazineSubTitleLabel.textColor = .gray
     
-        magazineDateLabel.text = date
+        magazineDateLabel.text = dateFormat(dateString: date)
         magazineDateLabel.textColor = .gray
-        magazineDateLabel.font = UIFont.systemFont(ofSize: 14)
+        magazineDateLabel.font = UIFont.systemFont(ofSize: 13)
         
         let url = URL(string: imageUrl)
         magazineImageView.kf.setImage(with: url)
         magazineImageView.contentMode = .scaleAspectFill
         magazineImageView.layer.cornerRadius = 12
-        
+    }
+    
+    // DateFormatter 이용한 날짜 변환
+    func dateFormat(dateString: String) -> String {
+        let dateFormatter = DateFormatter()
+        // 받아온 날짜의 형식
+        dateFormatter.dateFormat = "yyMMdd"
+        // String으로 받아온 날짜를 Date로 변환
+        guard let date = dateFormatter.date(from: dateString) else { return "" }
+        // 변환한 Date를 String으로 변경할 때의 형식
+        dateFormatter.dateFormat = "yy년 M월 d일"
+        // Date를 String으로 변환 후 return
+        return dateFormatter.string(from: date)
     }
 
 }
--- TableView_Travel/TravelTableViewController.swift
+++ TableView_Travel/TravelTableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class TravelTableViewController: UITableViewController {
 
@@ -13,19 +14,32 @@ class TravelTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 테이블뷰 내 셀의 높이를 콘텐츠의 크기에 맞게 자동으로 조절
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 450
+        // 셀 간 구분선 없애기
         tableView.separatorStyle = .none
     }
     
+    // 테이블뷰에 들어갈 셀의 개수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return magazineList.count
     }
     
+    // cell 구성
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineCell", for: indexPath) as? MagazineCell else { return UITableViewCell() }
         let magazine = magazineList[indexPath.row]
         cell.configure(title: magazine.title, subtitle: magazine.subtitle, date: magazine.date, imageUrl: magazine.photo_image)
+        
         return cell
+    }
+    
+    // 각 셀 탭했을 때 해당 링크(사파리) 띄우기 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath)
+        guard let url = URL(string: magazineList[indexPath.row].link) else { return }
+        let safari = SFSafariViewController(url: url)
+        present(safari, animated: true)
     }
 }
