cd6da52312af378330ae22c7c79b7ac4c1ff63fd
jeongyun-kim <kkanyo04@naver.com>
2024-05-30 16:07:50 +0900

    인기도시 검색 구현

--- TableView_Travel/PopularCity/PopularCityViewController.swift
+++ TableView_Travel/PopularCity/PopularCityViewController.swift
@@ -12,6 +12,8 @@ class PopularCityViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     
     let popularCityList = CityInfo.city
+    let korean = CityInfo.korean
+    let foreign = CityInfo.foreign
     
     var filteredCityList: [City] = []
     
@@ -20,8 +22,20 @@ class PopularCityViewController: UIViewController {
         setupTableView()
         setupSegmentControl()
         filteredCityList = popularCityList
+        setupSearchBar()
     }
+}
 
+// MARK: 서치바 / 테이블뷰 / 세그먼트 구성
+extension PopularCityViewController {
+    func setupSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "검색어를 입력해주세요"
+        searchController.hidesNavigationBarDuringPresentation = false
+        navigationItem.searchController = searchController
+        searchController.searchBar.delegate = self
+    }
+    
     func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
@@ -35,9 +49,6 @@ class PopularCityViewController: UIViewController {
     func setupSegmentControl() {
         let segmentTitles = ["모두", "국내", "해외"]
         let attribute = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .bold)]
-        
-        let korean = popularCityList.filter { $0.domestic_travel }
-        let foreign = popularCityList.filter { !$0.domestic_travel }
         let segmentActions = [popularCityList, korean, foreign]
         
         // 왜 액션이랑 타이틀 둘의 순서가 바뀌면 타이틀이 안 나오지?ㅅ?
@@ -59,6 +70,7 @@ class PopularCityViewController: UIViewController {
 }
 
 
+// MARK: 테이블뷰
 extension PopularCityViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredCityList.count
@@ -68,5 +80,42 @@ extension PopularCityViewController: UITableViewDelegate, UITableViewDataSource 
         guard let cell = tableView.dequeueReusableCell(withIdentifier: PopularCityTableViewCell.identifier, for: indexPath) as? PopularCityTableViewCell else { return UITableViewCell() }
         cell.configureCell(filteredCityList[indexPath.row])
         return cell
+    }
+}
+
+
+// MARK: 검색 구현
+extension PopularCityViewController: UISearchBarDelegate {
+    func search(_ searchBar: UISearchBar) {
+        guard let keyword = searchBar.text else { return }
+        
+        // 세그먼트 인덱스에 따라 다른 데이터로(모두/국내/해외)
+        switch segmentControl.selectedSegmentIndex {
+            case 0: filteredCityList = popularCityList
+            case 1: filteredCityList = korean
+            case 2: filteredCityList = foreign
+            default: filteredCityList = popularCityList
+        }
+        
+        // 소문자 처리한 검색어가 공백을 제거했을 때, 1글자 이상이라면 검색 돌리기
+        if keyword.lowercased().components(separatedBy: " ").joined().count > 0 {
+            let result = filteredCityList.filter {
+                $0.city_name.contains(keyword)
+                || $0.city_explain.contains(keyword)
+                || $0.city_english_name.lowercased().contains(keyword.lowercased())
+            }
+            filteredCityList = result
+        }
+        tableView.reloadData()
+    }
+    
+    // 실시간 검색
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        search(searchBar)
+    }
+    
+    // 검색 눌렀을 때
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        search(searchBar)
     }
 }
